88ef3ff872d49be73408f5f64c5e29aaf85d1518
Huldah <tientchen-huldah.chayo@stud.hs-hannover.de>
2025-06-13 12:54:58 +0200

    Anpassung an vertexShader

--- shaders/vertexShader.glsl
+++ shaders/vertexShader.glsl
@@ -7,7 +7,8 @@ layout (location = 3) in vec3 aTangent;
 out vec3 Position;
 out vec3 Normal;
 out vec2 TexCoord;
-out mat3 TBN; // Tangent, Bitangent, Normal matrix
+out mat3 TBN; // Tangent, Bitangent, Normal viewMatrix
+out vec3 LightDir;
 
 uniform vec3 lightPosition;
 
@@ -15,17 +16,23 @@ uniform mat4 modelMatrix;
 uniform mat4 viewMatrix;
 uniform mat3 normalMatrix;
 uniform mat4 projectionMatrix;
+uniform mat4 modelViewMatrix;
+uniform vec3 lightPosViewSpace;
+
+
 
 void main() {
-    vec4 viewPosition = viewMatrix * modelMatrix * aPosition;
+    vec4 viewPosition = modelViewMatrix * aPosition;
     Position = vec3(viewPosition);
-    Normal = vec3((normalMatrix * aNormal));
+    Normal = normalize(vec3(viewMatrix * vec4(aNormal, 0.0)));
     TexCoord = vec2(aTexCoord);
 
     vec3 T = normalize(vec3(modelMatrix * vec4(aTangent,   0.0)));
     vec3 N = normalize(vec3(modelMatrix * vec4(aNormal,    0.0)));
-    vec3 B = cross(N, T);
+    vec3 B = normalize(cross(N, T));
     TBN = mat3(T, B, N);
+    LightDir = normalize(lightPosViewSpace - Position);
+    LightDir = lightDir; 
     
     gl_Position = projectionMatrix * viewPosition;
 }
